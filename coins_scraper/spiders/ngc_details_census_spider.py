f7e08d0e6f1d4ad8aaef0e9b4930edfa5ea0e496
Ricky D <rickykeisling@gmail.com>
2023-06-29 13:21:46 -0500

    Split into two parse methods with callback, fix null name bug
    Currently working through a bug where the callback to parse_details is throwing an error ("Keywords must be strings")

--- coins_scraper/spiders/ngc_details_census_spider.py
+++ coins_scraper/spiders/ngc_details_census_spider.py
@@ -7,12 +7,11 @@ class NgcDetailsCensusSpider(scrapy.Spider):
     name = "ngc_details_census"
 
     start_urls = [
-            "https://www.ngccoin.com/details-census/world/germany-states-1871-1925/sc-144/5m/",
+            "https://www.ngccoin.com/details-census/world/germany-states-1871-1925/sc-144/5m/"
         ]
     
     def parse(self, response):
         page_title = response.css('div.sticky-header header h1::text').get() + response.css('div.sticky-header header h1 span::text').get()
-        narrow_detail_row = response.css('div.details-census div.scrollable table tbody tr.ms')
         broad_detail_row = response.css('div.details-census div.pinned table tbody tr.ms')
 
         # loops through each row, returns the first number in the row that isn't blank
@@ -29,14 +28,16 @@ class NgcDetailsCensusSpider(scrapy.Spider):
         #     print(first_data.get())
 
         broad_dict = {}
-        narrow_dict = {}
         counter = 0
         
         # biggun
         for row in broad_detail_row:
             name = row.css('td span span.merged::text').get()
             if name is None:
-                row.css('td.total-cell span::text').get()
+                print("name was null! name: " + str(name))
+                print("Attempting to get new name...")
+                name = row.css('td.total-cell span:nth-child(1)::text').get()
+                print("New name: " + str(name))
             denom = row.css('td:nth-child(2)::text').get()
             desig = row.xpath('//td[3]/text()').get()
             total_num = row.css('td.census-only::text').get()
@@ -52,19 +53,50 @@ class NgcDetailsCensusSpider(scrapy.Spider):
             
             broad_dict.update(sub_dict)
 
-        print("Broad dictionary: " + str(broad_dict))
+        # print("Broad dictionary: " + str(broad_dict))
+        
+        request = scrapy.Request(
+            "https://www.ngccoin.com/details-census/world/germany-states-1871-1925/sc-144/5m/data",
+            callback=self.parse_details,
+            cb_kwargs=broad_dict
+        )
+        
+        yield request
 
+        
+                    
+    def parse_details(self, response, broad_dict):
+        narrow_detail_row = response.css('body table tbody tr.ms')
+        
+        narrow_dict = {}
         counter = 0
         
         for row in narrow_detail_row:
             num_pr_ag_details = row.css('td.grade-PrAg::text').get()
+            if (num_pr_ag_details is None):
+                num_pr_ag_details = 0
             num_g_details = row.css('td.grade-G::text').get()
+            if (num_g_details is None):
+                num_g_details = 0
             num_vg_details = row.css('td.grade-VG::text').get()
+            if (num_vg_details is None):
+                num_vg_details = 0
             num_f_details = row.css('td.grade-F::text').get()
+            if (num_f_details is None):
+                num_f_details = 0
             num_vf_details = row.css('td.grade-VF::text').get()
+            if (num_vf_details is None):
+                num_vf_details = 0
             num_xf_details = row.css('td.grade-XF::text').get()
+            if (num_xf_details is None):
+                num_xf_details = 0
             num_au_details = row.css('td.grade-AU::text').get()
+            if (num_au_details is None):
+                num_au_details = 0
             num_unc_details = row.css('td.grade-UNC::text').get()
+            if (num_unc_details is None):
+                num_unc_details = 0
+            
             counter += 1
             
             sub_dict = {}
@@ -98,13 +130,13 @@ class NgcDetailsCensusSpider(scrapy.Spider):
         #                                "num_au_details": narrow_dict["num_au_details"],
         #                                "num_unc_details": narrow_dict["num_unc_details"]} for k in keys if broad_dict[k]["index"] == narrow_dict[k]["index"]}
         
-        print("Master dictionary: " + str(master))
+        # print("Master dictionary: " + str(master))
         
         for broad_key, broad_value in broad_dict.items():
             for narrow_key, narrow_value in narrow_dict.items():
                 if broad_key == narrow_key:
-                    yield {broad_value["name"]: {"denomination": broad_value["denomination"],
-                           "designation": broad_value["designation"],
+                    yield { broad_value["name"]: {"denomination": broad_value["denomination"],
+                            "designation": broad_value["designation"],
                             "total_number": broad_value["total_number"],
                             "num_pr_ag_details": narrow_value["num_pr_ag_details"],
                             "num_g_details": narrow_value["num_g_details"],
@@ -113,5 +145,4 @@ class NgcDetailsCensusSpider(scrapy.Spider):
                             "num_vf_details": narrow_value["num_vf_details"],
                             "num_xf_details": narrow_value["num_xf_details"],
                             "num_au_details": narrow_value["num_au_details"],
-                            "num_unc_details": narrow_value["num_unc_details"]}}
-                    
+                            "num_unc_details": narrow_value["num_unc_details"]} }
