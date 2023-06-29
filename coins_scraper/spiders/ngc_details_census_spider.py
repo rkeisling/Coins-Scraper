52dd1197fd2e6d2e4abbeded7a137c497a86e95f
Ricky D <rickykeisling@gmail.com>
2023-06-29 14:21:40 -0500

    Switch from nested for loops to dictionary comprehension
    Also cleanup and fix a couple of minor issues, like Total being overwritten and commas in totals

--- coins_scraper/spiders/ngc_details_census_spider.py
+++ coins_scraper/spiders/ngc_details_census_spider.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import scrapy
+import datetime
 
 
 class NgcDetailsCensusSpider(scrapy.Spider):
@@ -11,37 +12,23 @@ class NgcDetailsCensusSpider(scrapy.Spider):
         ]
     
     def parse(self, response):
-        page_title = response.css('div.sticky-header header h1::text').get() + response.css('div.sticky-header header h1 span::text').get()
         broad_detail_row = response.css('div.details-census div.pinned table tbody tr.ms')
-
-        # loops through each row, returns the first number in the row that isn't blank
-        # for row in narrow_detail_row:
-        #     print(row.css('td::text').get())
-        
-        # loops through, returns the data in the first column for each row
-        # for r in narrow_detail_row:
-        #     print(row.xpath('./td[0]//text()').get())
-        
-        # loops through, returns the name of the coin
-        # for row in broad_detail_row:
-        #     first_data = row.css('td span span.merged').xpath('.//text()')
-        #     print(first_data.get())
 
         broad_dict = {}
         counter = 0
         
-        # biggun
         for row in broad_detail_row:
             name = row.css('td span span.merged::text').get()
             if name is None:
-                print("name was null! name: " + str(name))
-                print("Attempting to get new name...")
                 name = row.css('td.total-cell span:nth-child(1)::text').get()
-                print("New name: " + str(name))
             denom = row.css('td:nth-child(2)::text').get()
             desig = row.css('td:nth-child(3)::text').get()
-            total_num = row.css('td.census-only::text').get()
+            total_num = row.css('td.census-only::text').get().replace(",", "").replace(" ", "")
+            origin = response.request.url
             counter += 1
+            
+            if name == "Total":
+                name = name + " - " + desig
             
             sub_dict = {}
             
@@ -49,12 +36,11 @@ class NgcDetailsCensusSpider(scrapy.Spider):
                                  "denomination": denom,
                                  "designation": desig,
                                  "total_number": total_num,
+                                 "origin": origin,
                                  "index": counter}
             
             broad_dict.update(sub_dict)
 
-        # print("Broad dictionary: " + str(broad_dict))
-        
         request = scrapy.Request(
             "https://www.ngccoin.com/details-census/world/germany-states-1871-1925/sc-144/5m/data",
             callback=self.parse_details,
@@ -113,36 +99,39 @@ class NgcDetailsCensusSpider(scrapy.Spider):
     
             narrow_dict.update(sub_dict)
         
-        print("Narrow dictionary: " + str(narrow_dict))
+        keys = set(broad_dict.keys()) & set(narrow_dict.keys())
         
-        # keys = set(broad_dict.keys()) & set(narrow_dict.keys())
+        # dictionary comprehension method
+        # is faster than the nested loops
+        yield { broad_dict[k]["name"]: {"denomination": broad_dict[k]["denomination"],
+                                        "designation": broad_dict[k]["designation"],
+                                        "total_number": broad_dict[k]["total_number"],
+                                        "num_pr_ag_details": narrow_dict[k]["num_pr_ag_details"],
+                                        "num_g_details": narrow_dict[k]["num_g_details"],
+                                        "num_vg_details": narrow_dict[k]["num_vg_details"],
+                                        "num_f_details": narrow_dict[k]["num_f_details"],
+                                        "num_vf_details": narrow_dict[k]["num_vf_details"],
+                                        "num_xf_details": narrow_dict[k]["num_xf_details"],
+                                        "num_au_details": narrow_dict[k]["num_au_details"],
+                                        "num_unc_details": narrow_dict[k]["num_unc_details"],
+                                        "origin": broad_dict[k]["origin"],
+                                        "last_updated_at": str(datetime.datetime.now())} for k in keys if broad_dict[k]["index"] == narrow_dict[k]["index"]}
         
-        # trying to figure out how to do this dictionary comprehension
-        # master = {broad_dict["name"]: {"denomination": broad_dict["denomination"],
-        #                                "designation": broad_dict["designation"],
-        #                                "total_number": broad_dict["total_number"],
-        #                                "num_pr_ag_details": narrow_dict["num_pr_ag_details"],
-        #                                "num_g_details": narrow_dict["num_g_details"],
-        #                                "num_vg_details": narrow_dict["num_vg_details"],
-        #                                "num_f_details": narrow_dict["num_f_details"],
-        #                                "num_vf_details": narrow_dict["num_vf_details"],
-        #                                "num_xf_details": narrow_dict["num_xf_details"],
-        #                                "num_au_details": narrow_dict["num_au_details"],
-        #                                "num_unc_details": narrow_dict["num_unc_details"]} for k in keys if broad_dict[k]["index"] == narrow_dict[k]["index"]}
         
-        # print("Master dictionary: " + str(master))
-        
-        for broad_key, broad_value in broad_dict.items():
-            for narrow_key, narrow_value in narrow_dict.items():
-                if broad_key == narrow_key:
-                    yield { broad_value["name"]: {"denomination": broad_value["denomination"],
-                            "designation": broad_value["designation"],
-                            "total_number": broad_value["total_number"],
-                            "num_pr_ag_details": narrow_value["num_pr_ag_details"],
-                            "num_g_details": narrow_value["num_g_details"],
-                            "num_vg_details": narrow_value["num_vg_details"],
-                            "num_f_details": narrow_value["num_f_details"],
-                            "num_vf_details": narrow_value["num_vf_details"],
-                            "num_xf_details": narrow_value["num_xf_details"],
-                            "num_au_details": narrow_value["num_au_details"],
-                            "num_unc_details": narrow_value["num_unc_details"]} }
+        # nested loops method
+        # for broad_key, broad_value in broad_dict.items():
+        #     for narrow_key, narrow_value in narrow_dict.items():
+        #         if broad_key == narrow_key:
+        #             yield { broad_value["name"]: {"denomination": broad_value["denomination"],
+        #                     "designation": broad_value["designation"],
+        #                     "total_number": broad_value["total_number"],
+        #                     "origin": broad_value["origin"],
+        #                     "num_pr_ag_details": narrow_value["num_pr_ag_details"],
+        #                     "num_g_details": narrow_value["num_g_details"],
+        #                     "num_vg_details": narrow_value["num_vg_details"],
+        #                     "num_f_details": narrow_value["num_f_details"],
+        #                     "num_vf_details": narrow_value["num_vf_details"],
+        #                     "num_xf_details": narrow_value["num_xf_details"],
+        #                     "num_au_details": narrow_value["num_au_details"],
+        #                     "num_unc_details": narrow_value["num_unc_details"],
+        #                     "last_updated_at": str(datetime.datetime.now())} }
