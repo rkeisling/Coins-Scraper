ad5d4e051816ac35092cabfdf04cc0584f01e72b
Ricky D <rickykeisling@gmail.com>
2023-06-30 10:20:18 -0500

    Turn on autothrottling

--- coins_scraper/settings.py
+++ coins_scraper/settings.py
@@ -27,8 +27,9 @@ ROBOTSTXT_OBEY = True
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 3
+CONCURRENT_REQUESTS_PER_IP = 3
+AUTOTHROTTLE_ENABLED = True
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
